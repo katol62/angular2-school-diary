a0c687cdfa2b1d7be51956b62564d1de41f7acb4
VictorKozachek <victor.kozachek@yandex.ru>
2017-03-23 17:42:54 +0300

    New stream url

--- frontend/src/client/app/dashboard/system/system.component.ts
+++ frontend/src/client/app/dashboard/system/system.component.ts
@@ -15,7 +15,7 @@ export class SystemComponent implements OnInit {
 
     @Input() user:User;
 
-    webvideo:any = {idx: '1', src: 'http://staging.schoolarlife.ru:8090/feed.webm', type: 'video/webm'};
+    webvideo:any = {idx: '1', src: 'http://staging.schoolarlife.ru:8090/cam.webm', type: 'video/webm'};
 
     constructor(private globalEventManager:GlobalEventsManager) { }
 
